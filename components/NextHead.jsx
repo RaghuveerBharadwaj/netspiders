58d84d953099fd11dce038cb33df3490d0664672
Gandhi S R <89028457+gandhisr03@users.noreply.github.com>
2021-08-16 21:40:48 +0530

    Update NextHead.jsx

--- components/NextHead.jsx
+++ components/NextHead.jsx
@@ -4,7 +4,8 @@ import React from 'react'
 export const NextHead = () => {
   return (
     <Head>
-      <title>Web Development | Digital Marketing Services</title>
+      <title>Digital Marketing Agency Mysore | Netspiders </title>
+    <meta name="description" content="Best digital marketing agency in Mysore. We provider-SEO services, PPC services, Social media marketing and Web design and development services.">
       <link rel="icon" href="/favicon.ico" />
     </Head>
   )
