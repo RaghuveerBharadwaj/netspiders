858b800c0157700cd7bf4e3bd1b4889e7f1d586b
Gandhi S R <89028457+gandhisr03@users.noreply.github.com>
2021-08-20 21:53:41 +0530

    Update Landing.jsx

--- components/Landing.jsx
+++ components/Landing.jsx
@@ -12,7 +12,7 @@ export const Landing = () => {
              Digital Marketing Agency Mysore | Netspiders
             </h1>
           <p>
-          Web development is the work involved in developing a Web site for the Internet or an intranet. Web development can range from developing a simple single static page of plain text to complex Web-based Internet applications, electronic businesses, and social network.
+          Web development is the work involved in developing a Web site for the Internet or an intranet. Web development can range from developing a simple single static page of plain text to complex Web-based Internet applications, electronic businesses, and social network, being the best didital marketing agency in mysore, we offer customized service to enhance our clients growth in the digital platforms.
           </p>
           </Zoom>
         </div>
