8269d6868aa7a80046a1ce605ca0c5a2e54dd512
Gandhi S R <89028457+gandhisr03@users.noreply.github.com>
2021-08-20 21:38:39 +0530

    Update NextHead.jsx

--- components/NextHead.jsx
+++ components/NextHead.jsx
@@ -8,6 +8,15 @@ export const NextHead = () => {
     <meta name="description" content="Best digital marketing agency in Mysore. We provider-SEO services, PPC services, Social media marketing and Web design and development services." />
       <link rel="icon" href="/favicon.ico" />
     <meta name="google-site-verification" content="sScjrwL9_XG827P0Sl_odbasOi6MBv2qhCL8M5rLLac" />
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-0WQB5VDFYN"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-0WQB5VDFYN');
+</script>
     </Head>
   )
 }
